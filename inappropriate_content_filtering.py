1dcf896973180f244687c09f23107b452e8c4285
vighneshsridhar <133550170+vighneshsridhar@users.noreply.github.com>
2024-10-30 15:47:19 -0700

    Update inappropriate_content_filtering.py

--- inappropriate_content_filtering.py
+++ inappropriate_content_filtering.py
@@ -33,40 +33,6 @@ def home():
 #from google.colab import drive
 #drive.mount('/content/gdrive')
 
-tweet_data = pd.read_csv('labeled_data.csv', encoding = "utf-8")
-print(tweet_data.shape)
-
-tweet_data.columns
-
-tweet_data.head()
-
-tweet_data.isna().sum()
-
-tweet_data.describe()
-
-for col in tweet_data[['count', 'hate_speech', 'offensive_language', 'neither',
-       'class']]:
-    sns.histplot(tweet_data[col])
-    plt.show()
-
-tweet_data[tweet_data['hate_speech']>0].describe()
-
-tweet_data[tweet_data['offensive_language']>0].describe()
-
-tweet_data[tweet_data['neither']>0].describe()
-
-tweet_data['class'].hist()
-
-tweet_data['sentiment'] = tweet_data['class'].map({0:'Hate_Speech',1:'offensive_language',
-                                  2: 'Neither'})
-
-
-fig, axs = plt.subplots(figsize=(6,5))
-sns.countplot(x='sentiment',data = tweet_data,ax=axs)
-axs.set_xticklabels(axs.get_xticklabels(),rotation=40,ha="right")
-plt.tight_layout()
-plt.show()
-
 @app.route('/api/clean_text', methods=['POST'])
 def clean_text(text):
 
@@ -87,9 +53,6 @@ def clean_text(text):
 
     return text
 
-tweet_data['tweet'] = tweet_data['tweet'].apply(clean_text)
-tweet_data['tweet'].head(10)
-
 sw = set(stopwords.words("english"))
 
 @app.route('/api/remove_stopwords', methods=['POST'])
@@ -98,10 +61,7 @@ def remove_stopwords(text):
     cleaned_tokens = [word for word in tokens if word.lower() not in sw]
     return " ".join(cleaned_tokens)
 
-
-tweet_data['tweet'] = tweet_data['tweet'].apply(remove_stopwords)
-tweet_data['tweet'].head(10)
-
+tweet_data = pd.read_csv('labeled_data.csv', encoding = "utf-8")
 text = " ".join(i for i in tweet_data['tweet'])
 
 
@@ -149,10 +109,48 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 # Create a TfidfVectorizer object
 vectorizer = TfidfVectorizer()
 
-X = tweet_data['tweet']
-y = tweet_data['class']
+def create_data():
+    tweet_data = pd.read_csv('labeled_data.csv', encoding = "utf-8")
+    print(tweet_data.shape)
 
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=.20,random_state=42)
+    tweet_data.columns
+
+    tweet_data.head()
+
+    tweet_data.isna().sum()
+
+    tweet_data.describe()
+
+    for col in tweet_data[['count', 'hate_speech', 'offensive_language', 'neither',
+           'class']]:
+        sns.histplot(tweet_data[col])
+        plt.show()
+
+    tweet_data[tweet_data['hate_speech']>0].describe()
+
+    tweet_data[tweet_data['offensive_language']>0].describe()
+
+    tweet_data[tweet_data['neither']>0].describe()
+
+    tweet_data['class'].hist()
+
+    tweet_data['sentiment'] = tweet_data['class'].map({0:'Hate_Speech',1:'offensive_language',
+                                      2: 'Neither'})
+    fig, axs = plt.subplots(figsize=(6,5))
+    sns.countplot(x='sentiment',data = tweet_data,ax=axs)
+    axs.set_xticklabels(axs.get_xticklabels(),rotation=40,ha="right")
+    plt.tight_layout()
+    plt.show()
+    
+
+    tweet_data['tweet'] = tweet_data['tweet'].apply(clean_text)
+    tweet_data['tweet'] = tweet_data['tweet'].apply(remove_stopwords)
+    X = tweet_data['tweet']
+    y = tweet_data['class']
+
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=.20,random_state=42)
+
+    return X_train, X_test, y_train, y_test
 
 from sklearn.linear_model import LogisticRegression
 lr = LogisticRegression()
@@ -171,19 +169,19 @@ def evaluate_model(X_train,X_test,y_train,y_test,model):
     print(f'{model.__class__.__name__}, --Accuracy-- {acc*100:.2f}%; --Clf RPT-- {clf_rpt}')
     return pred
 
-lr_pred = evaluate_model(X_train, X_test, y_train, y_test, lr)
+#lr_pred = evaluate_model(X_train, X_test, y_train, y_test, lr)
 
 @app.route('/api/classify_text', methods=['POST'])
 def classify_text():
     data = request.json
-    clean_data = clean_text(data)
+    clean_data = clean_text(data['text'])
     text = remove_stopwords(clean_data)
-    X_test = data['text']
+    X_train, X_test, y_train, y_test = create_data()
     model = model.fit(X_train, y_train)
-    pred = model.predict(X_test)
+    pred = model.predict(text)
 
     return jsonify(pred), 200
-    
+
 
 
 if __name__ in "main":
